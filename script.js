2abee2df5ccd70c16a254affa4db143da74a7be4
Gokulraj Tamilvindan <97329414+gokulrajtamilvindan@users.noreply.github.com>
2025-09-26 10:38:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,16 +1,28 @@
-//your JS code here. If required.
 function stripArticles(article) {
-    return article.replace(/^(a |an |the )/i, '');
+  return article.replace(/^(a |an |the )/i, '').trim();
 }
 
-const bands = ['The Plot in You', 'The Devil Wears Prada', 'Pierce the Veil', 'Norma Jean', 'The Bled', 'Say Anything', 'The Midway State', 'We Came as Romans', 'Counterparts', 'Oh, Sleeper', 'A Skylit Drive', 'Anywhere But Here', 'An Old Dog'];
-bands.sort((a, b) => {
-    stripArticles(a) > stripArticles(b) ? 1 : -1;
-})
+const bands = [
+  'The Plot in You',
+  'The Devil Wears Prada',
+  'Pierce the Veil',
+  'Norma Jean',
+  'The Bled',
+  'Say Anything',
+  'The Midway State',
+  'We Came as Romans',
+  'Counterparts',
+  'Oh, Sleeper',
+  'A Skylit Drive',
+  'Anywhere But Here',
+  'An Old Dog'
+];
+
+bands.sort((a, b) => stripArticles(a).localeCompare(stripArticles(b)));
 
 const ul = document.getElementById('bands');
 bands.forEach(band => {
-    const li = document.createElement('li');
-    li.innerText = band;
-    ul.appendChild(li);
+  const li = document.createElement('li');
+  li.textContent = band;
+  ul.appendChild(li);
 });
